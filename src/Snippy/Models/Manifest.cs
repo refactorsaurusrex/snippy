3de9762a251115f013ccfea67a13b315e4e4c292
Nick Spreitzer <nick@200-ok.com>
2020-12-04 23:44:34 -0600

    fix(manifest): Merge existing manifest data with new manifest

--- src/Snippy/Models/Manifest.cs
+++ src/Snippy/Models/Manifest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using YamlDotNet.Serialization;
 
@@ -14,14 +15,27 @@ namespace Snippy.Models
         {
             var deserializer = new Deserializer();
             var path = Path.Combine(workspaceDirectory, FileName);
-            var text = File.ReadAllText(path);
-            return deserializer.Deserialize<Manifest>(text);
+            try
+            {
+                var text = File.ReadAllText(path);
+                return deserializer.Deserialize<Manifest>(text) ?? new Manifest();
+            }
+            catch (Exception)
+            {
+                return new Manifest();
+            }
         }
 
         public List<WorkspaceDefinition> Definitions { get; set; } = new List<WorkspaceDefinition>();
 
         public void Publish(string workspaceDirectory)
         {
+            var oldManifest = Load(workspaceDirectory);
+
+            foreach (var old in oldManifest.Definitions.Where(old => Definitions.SingleOrDefault(current => current.FileName == old.FileName) == null))
+                Definitions.Add(old);
+
+            Definitions = Definitions.OrderBy(x => x.FileName).ToList();
             var serializer = new Serializer();
             using var manifestFile = File.CreateText(Path.Combine(workspaceDirectory, FileName));
             serializer.Serialize(manifestFile, this);
