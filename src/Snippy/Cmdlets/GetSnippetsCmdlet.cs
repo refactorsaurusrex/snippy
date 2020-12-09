717fe9b1a3a57ab8d89b7345751f97be38e4b272
Nick Spreitzer <nick@200-ok.com>
2020-12-08 23:47:14 -0600

    improvement: Declare output type

--- src/Snippy/Cmdlets/GetSnippetsCmdlet.cs
+++ src/Snippy/Cmdlets/GetSnippetsCmdlet.cs
@@ -1,11 +1,14 @@
-﻿using System.Management.Automation;
+﻿using System.Collections.Generic;
+using System.Management.Automation;
 using JetBrains.Annotations;
+using Snippy.Models;
 using Snippy.Services;
 
 namespace Snippy.Cmdlets
 {
     [PublicAPI]
     [Cmdlet(VerbsCommon.Get, "Snippets")]
+    [OutputType(typeof(ICollection<SnippetIndexEntry>))]
     public class GetSnippetsCmdlet : CmdletBase
     {
         protected override void Run()
