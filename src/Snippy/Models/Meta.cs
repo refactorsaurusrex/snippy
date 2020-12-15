57415dfa6ffd5ac5d127085043c4977be4b038e6
Nick Spreitzer <nick@200-ok.com>
2020-12-15 14:25:29 -0800

    fix(meta): Initialize meta tags to empty list, not null

--- src/Snippy/Models/Meta.cs
+++ src/Snippy/Models/Meta.cs
@@ -5,10 +5,17 @@ namespace Snippy.Models
     internal class Meta
     {
         public static string FileName = "meta.yaml";
+        private List<string> _tags;
 
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<string> Tags { get; set; }
+
+        public List<string> Tags
+        {
+            get => _tags;
+            set => _tags = value ?? new List<string>();
+        }
+
         public string GistUrl { get; set; }
     }
 }
