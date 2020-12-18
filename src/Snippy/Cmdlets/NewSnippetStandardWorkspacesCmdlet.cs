f359c374434e59888b9b72e51e1c12783b1df1a4
Nick Spreitzer <nick@200-ok.com>
2020-12-18 15:56:18 -0800

    feat!: Remove workspace type option for new standard workspaces
    BREAKING CHANGE: Instead of allowing the user to specify a workspace type to create, the New-SnippetStandardWorkspaces will now just create all types by default.

---
+++ src/Snippy/Cmdlets/NewSnippetStandardWorkspacesCmdlet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Management.Automation;
+using JetBrains.Annotations;
+using Snippy.Models;
+using Snippy.Services;
+
+namespace Snippy.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.New, "SnippetStandardWorkspaces")]
+    public class NewSnippetStandardWorkspacesCmdlet : CmdletBase
+    {
+        [Parameter]
+        public OrderBy OrderBy { get; set; }
+
+        [Parameter]
+        public SortDirection SortDirection { get; set; }
+
+        [Parameter]
+        public SwitchParameter HideMetaFiles { get; set; }
+
+        [Parameter]
+        public SwitchParameter Overwrite { get; set; }
+
+        [Parameter]
+        public SwitchParameter Sync { get; set; }
+
+        protected override void Run()
+        {
+            var organizer = new SnippetOrganizer(Options, FileAssociations);
+            var manifestGenerator = new ManifestGenerator();
+
+            var unpartitioned = organizer.CreateUnpartitionedWorkspace(OrderBy, SortDirection, HideMetaFiles);
+            try
+            {
+                unpartitioned.Publish(Options, Overwrite);
+                manifestGenerator.Add(unpartitioned);
+            }
+            catch (InvalidOperationException e)
+            {
+                WriteWarning(e.Message);
+            }
+
+            var languagePackages = organizer.CreateWorkspacesByLanguage(OrderBy, SortDirection, HideMetaFiles);
+            foreach (var languagePackage in languagePackages)
+            {
+                try
+                {
+                    languagePackage.Publish(Options, Overwrite);
+                    manifestGenerator.Add(languagePackage);
+                }
+                catch (InvalidOperationException e)
+                {
+                    WriteWarning(e.Message);
+                }
+            }
+
+            var tagPackages = organizer.CreateWorkspacesByTag(OrderBy, SortDirection, HideMetaFiles);
+            foreach (var tagPackage in tagPackages)
+            {
+                try
+                {
+                    tagPackage.Publish(Options, Overwrite);
+                    manifestGenerator.Add(tagPackage);
+                }
+                catch (InvalidOperationException e)
+                {
+                    WriteWarning(e.Message);
+                }
+            }
+
+            var manifest = manifestGenerator.ToManifest(OrderBy, SortDirection);
+            manifest.Publish(Options.WorkspacePath);
+
+            if (Sync || Options.AutoSync)
+                CommitAndPush("New standard workspace");
+        }
+    }
+}
