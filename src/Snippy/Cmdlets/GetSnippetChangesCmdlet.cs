40500c747ac83f7e80a6c3c10f7695c59ef4f6ed
Nick Spreitzer <nick@200-ok.com>
2020-12-19 12:50:50 -0800

    improve(git): If error encountered, return error message

--- src/Snippy/Cmdlets/GetSnippetChangesCmdlet.cs
+++ src/Snippy/Cmdlets/GetSnippetChangesCmdlet.cs
@@ -11,7 +11,15 @@ namespace Snippy.Cmdlets
         {
             using var ps = PowerShell.Create();
             var getRoot = @$"cd {Options.SnippetPath}; git rev-parse --show-toplevel;";
-            var root = ps.AddScript(getRoot).Invoke()[0].BaseObject as string;
+            var response = ps.AddScript(getRoot).Invoke();
+
+            if (ps.HadErrors)
+            {
+                WriteObject(ps.Streams.Error);
+                return;
+            }
+
+            var root = response[0].BaseObject as string;
             var path = root ?? Options.SnippetPath;
             var getStatus = @$"cd {path}; git status;";
             var status = ps.AddScript(getStatus).Invoke();
